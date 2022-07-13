b5829cb71094170bf93823662c31e55ded213097
trymeuj <108957865+trymeuj@users.noreply.github.com>
2022-07-14 03:14:27 +0530

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -6,9 +6,9 @@ model_nb = joblib.load('Store_Sales')
 
 # 3 user input 
 st.title("Store Sales Predict")
-ip1 = st.text_input("Store_Area")
-ip2 = st.text_input("Items_Available")
-ip3 = st.text_input("Daily_Customer_Count")
+ip1 = st.number_input("Store_Area")
+ip2 = st.number_input("Items_Available")
+ip3 = st.number_input("Daily_Customer_Count")
 
 
 op = model_nb.predict([[ip1,ip2,ip3]])
