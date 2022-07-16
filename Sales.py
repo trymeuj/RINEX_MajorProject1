7b804935f9103490dfd70745878ce26e42a636f1
trymeuj <108957865+trymeuj@users.noreply.github.com>
2022-07-16 23:07:10 +0530

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 import joblib 
-import MinMaxScaler
-scaling = MinMaxScaler()
+
 
 #load the already uploaded joblib model 
 model_nb = joblib.load('Store_Sales')
@@ -11,8 +10,8 @@ st.title("Store Sales Predict")
 ip1 = st.number_input("Store_Area")
 ip2 = st.number_input("Items_Available")
 ip3 = st.number_input("Daily_Customer_Count")
-a = scaling.fit_transform([[ip1,ip2,ip3]])
 
-op = model_nb.predict(a)
+
+op = model_nb.predict([[ip1,ip2,ip3]])
 if st.button('PREDICT'):
   st.title(op[0])  #prints predicted store sales
